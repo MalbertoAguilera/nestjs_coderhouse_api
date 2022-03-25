58ab6a612d0974159e82e341a8fedd93973ad80a
Matias Aguilera <malbertoaguilera@outlook.es>
2022-03-24 23:48:27 -0300

    rutas getAll, getById, addProduct listo

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,14 +1,44 @@
-import { Controller, Get, Post, Put, Delete, Res, HttpStatus, Body} from '@nestjs/common';
-import {CreateProductDTO} from './dto/product.dto'
+import { Controller, Get, Post, Put, Delete, Res, HttpStatus, Body, Param, NotFoundException} from '@nestjs/common';
+import { get } from 'http';
+import {CreateProductDTO} from './dto/product.dto';
+import { ProductService } from './product.service';
 
 @Controller('product')
 export class ProductController {
 
+      constructor(private productService:ProductService){}
+
       @Post('/create')
-      createPost(@Res() res, @Body() createProductDTO: CreateProductDTO){
-            console.log(createProductDTO);
-            res.status(HttpStatus.OK).json({
-                  message:'received'
+      async createPost(@Res() res, @Body() createProductDTO: CreateProductDTO){
+            const product = await this.productService.createProduct(createProductDTO);
+            return res.status(HttpStatus.OK).json({
+                  message:'product successfully created',
+                  product
+            });
+      }
+
+      @Get('/')
+      async getProducts(@Res() res) {
+            const products = await this.productService.getProducts();
+            return res.status(HttpStatus.OK).json({
+                  products
             })
       }
+
+      @Get('/:productID')
+      async getProduct(@Res() res, @Param('productID') productID) {
+            const product = await this.productService.getProduct(productID);
+            if(!product){
+                  throw new NotFoundException('product desn`t exists')
+            }
+            return res.status(HttpStatus.OK).json({
+                  product
+            })
+      }
+
+      // @Delete('/delete')
+      // deleteProduct(){}
+
+      // @Put('/delete')
+      // updateProduct(){}
 }
